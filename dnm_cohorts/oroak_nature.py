1bc621d10cc7c00d827bff355829c2bf2dc80b53
Jeremy McRae <jmcrae@illumina.com>
2018-04-17 13:23:14 -0700

    don't use final row from oroak paper

--- dnm_cohorts/oroak_nature.py
+++ dnm_cohorts/oroak_nature.py
@@ -6,7 +6,8 @@ from dnm_cohorts.person import Person
 url = 'https://media.nature.com/original/nature-assets/nature/journal/v485/n7397/extref/nature10989-s2.xls'
 
 def open_oroak_cohort():
-    data = pandas.read_excel(url, sheet_name='Supplementary Table 1')
+    data = pandas.read_excel(url, sheet_name='Supplementary Table 1',
+        skipfooter=1)
     
     study = 'oroak_nature_2012'
     status = 'autism'
