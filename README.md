a892a32cd473ea6674e2937362df1a1fcba06fbd
Jeremy McRae <jmcrae@illumina.com>
2018-05-01 16:33:38 -0700

    swap entry point around so we can also collect de novo mutations

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 
 #### dnm_cohorts
 
-This small package collects sample information from published studies of de novo
-mutations in rare disease. This doesn't collect any of the de novo mutations,
-but instead focuses on identifying all individuals in the studies, so that we
-can have an accurate numbers of individuals for different phenotypes. This also
-attempts to reconcile individuals included in multiple studies, so we don't
-double count individuals if they are present in multiple studies.
+This small package collects sample information and de novo mutations from
+published studies of de novo mutations in rare disease.
+
+This also reconciles individuals and variants included in multiple studies, so
+we don't double count individuals if they are present in multiple studies.
 
 All of the information is obtained from the relevant supplementary data tables
 associated with each journal publication.
@@ -18,5 +17,9 @@ pip install git+git://git.illumina.com/jeremymcrae/dnm_cohorts.git
 
 #### Run
 ``` sh
-dnm_cohorts --output test.txt
+# to create a table of all individuals in the cohorts
+dnm_cohorts --cohorts --output test.txt
+
+# to create a table of all de novo mutations found in those individuals
+dnm_cohorts --de-novos --output test.txt
 ```
--- dnm_cohorts/__main__.py
+++ dnm_cohorts/__main__.py
@@ -3,14 +3,24 @@ import itertools
 import argparse
 import sys
 
+from dnm_cohorts.ensembl import cq_and_symbol
 from dnm_cohorts.cohorts import (open_de_ligt_cohort, open_rauch_cohort,
     open_de_rubeis_cohort, open_epi4k_nature_cohort, open_iossifov_nature_cohort,
     open_iossifov_neuron_cohort, open_oroak_cohort, open_sanders_nature_cohort,
     open_sanders_neuron_cohort, open_lelieveld_cohort, open_mcrae_nature_cohort)
+from dnm_cohorts.de_novos import (de_ligt_nejm_de_novos,
+    de_rubeis_nature_de_novos, epi4k_ashg_de_novos, gilissen_nature_de_novos,
+    iossifov_neuron_de_novos, iossifov_nature_de_novos, lelieveld_nn_de_novos,
+    mcrae_nature_de_novos, oroak_nature_de_novos, rauch_lancet_de_novos,
+    sanders_nature_de_novos, sanders_neuron_de_novos)
 
 def get_options():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output', default=sys.stdout, help='path to save output to')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--de-novos', default=False, action='store_true')
+    group.add_argument('--cohorts', default=False, action='store_true')
+    
+    parser.add_argument('--output', default=sys.stdout, help='where to save output')
     
     args = parser.parse_args()
     
@@ -21,36 +31,59 @@ def get_options():
     
     return args
 
-def main():
-    args = get_options()
+def open_cohorts():
+    ''' get list of all individuals in all cohorts
+    '''
+    # open ASD cohort info, then drop duplicate samples from the ASD cohorts
+    asd = [open_de_rubeis_cohort(), open_iossifov_nature_cohort(),
+        open_iossifov_neuron_cohort(), open_oroak_cohort(),
+        open_sanders_nature_cohort(), open_sanders_neuron_cohort()]
     
-    de_ligt = open_de_ligt_cohort()
-    rauch_lancet = open_rauch_cohort()
-    de_rubeis = open_de_rubeis_cohort()
-    epi4k = open_epi4k_nature_cohort()
-    iossifov_nature = open_iossifov_nature_cohort()
-    iossifov_neuron = open_iossifov_neuron_cohort()
-    oroak_nature = open_oroak_cohort()
-    sanders_nature = open_sanders_nature_cohort()
-    sanders_neuron = open_sanders_neuron_cohort()
-    lelieveld_nn = open_lelieveld_cohort()
-    mcrae_nature = open_mcrae_nature_cohort()
-    
-    # drop duplicate samples from the ASD cohorts
-    asd_cohorts = [de_rubeis, iossifov_nature, iossifov_neuron, oroak_nature,
-        sanders_nature, sanders_neuron]
-        
-    for a, b in itertools.combinations(asd_cohorts, 2):
+    for a, b in itertools.combinations(asd, 2):
         a -= b
     
     # TODO: what about the 1 in 1000 people with differing sex between studies?
     # TODO: I've kept them for now, since they are negligle and conservative.
-    samples = asd_cohorts + [de_ligt, rauch_lancet, epi4k, lelieveld_nn, mcrae_nature]
+    samples = asd + [open_de_ligt_cohort(), open_rauch_cohort(),
+        open_epi4k_nature_cohort(), open_lelieveld_cohort(),
+        open_mcrae_nature_cohort()]
     
-    _ = args.output.write('person_id\tsex\tphenotype\tstudy\n')
-    for cohort in samples:
-        for sample in cohort:
-            _ = args.output.write(str(sample))
+    return samples
+
+def open_de_novos():
+    """ get list of all de novos in all cohorts
+    """
+    # open ASD cohort info, then drop duplicate samples from the ASD cohorts
+    asd = [de_rubeis_nature_de_novos(), iossifov_neuron_de_novos(),
+        iossifov_nature_de_novos(), oroak_nature_de_novos(),
+        sanders_nature_de_novos(), sanders_neuron_de_novos()]
+    for a, b in itertools.combinations(asd, 2):
+        a -= b
+    
+    cohorts = asd + [de_ligt_nejm_de_novos(), gilissen_nature_de_novos(),
+        epi4k_ashg_de_novos(), lelieveld_nn_de_novos(), rauch_lancet_de_novos(),
+        mcrae_nature_de_novos()]
+    
+    return cohorts
+
+def main():
+    args = get_options()
+    
+    if args.cohorts:
+        data = open_cohorts()
+        header = ['person_id', 'sex', 'phenotype', 'study']
+    else:
+        data = open_de_novos()
+        header = ['person_id', 'chrom', 'pos', 'ref', 'alt', 'study',
+            'confidence', 'symbol', 'consequence']
+    
+    _ = args.output.write('\t'.join(header) + '\n')
+    for cohort in data:
+        for x in cohort:
+            if args.de_novos:
+                x.consequence, x.symbol = cq_and_symbol(x.chrom, x.pos, x.ref, x.alt)
+            
+            _ = args.output.write(str(x) + '\n')
 
 if __name__ == '__main__':
     main()
