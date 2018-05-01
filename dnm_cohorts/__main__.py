fe1010526cfdf683c87acff0df93c333b66ac9a8
Jeremy McRae <jmcrae@illumina.com>
2018-05-01 11:14:18 -0700

    simplify imports

--- dnm_cohorts/__main__.py
+++ dnm_cohorts/__main__.py
@@ -3,17 +3,10 @@ import itertools
 import argparse
 import sys
 
-from dnm_cohorts.cohorts.de_ligt_nejm import open_de_ligt_cohort
-from dnm_cohorts.cohorts.rauch_lancet import open_rauch_cohort
-from dnm_cohorts.cohorts.de_rubeis_nature import open_de_rubeis_cohort
-from dnm_cohorts.cohorts.epi4k_nature import open_epi4k_nature_cohort
-from dnm_cohorts.cohorts.iossifov_nature import open_iossifov_nature_cohort
-from dnm_cohorts.cohorts.iossifov_neuron import open_iossifov_neuron_cohort
-from dnm_cohorts.cohorts.oroak_nature import open_oroak_cohort
-from dnm_cohorts.cohorts.sanders_nature import open_sanders_nature_cohort
-from dnm_cohorts.cohorts.sanders_neuron import open_sanders_neuron_cohort
-from dnm_cohorts.cohorts.lelieveld_nn import open_lelieveld_cohort
-from dnm_cohorts.cohorts.mcrae_nature import open_mcrae_nature_cohort
+from dnm_cohorts.cohorts import (open_de_ligt_cohort, open_rauch_cohort,
+    open_de_rubeis_cohort, open_epi4k_nature_cohort, open_iossifov_nature_cohort,
+    open_iossifov_neuron_cohort, open_oroak_cohort, open_sanders_nature_cohort,
+    open_sanders_neuron_cohort, open_lelieveld_cohort, open_mcrae_nature_cohort)
 
 def get_options():
     parser = argparse.ArgumentParser()
