60d02af7adead4c9793771cada596d7916ff48af
Jeremy McRae <jmcrae@illumina.com>
2018-05-08 11:25:38 -0700

    fix EPI4K IDs in de novo table

--- dnm_cohorts/de_novos/epi4k_ashg.py
+++ dnm_cohorts/de_novos/epi4k_ashg.py
@@ -8,6 +8,30 @@ from dnm_cohorts.de_novo import DeNovo
 
 url = 'https://ars.els-cdn.com/content/image/1-s2.0-S0002929714003838-mmc2.xlsx'
 
+def get_person_ids(data):
+    """ lean up the person IDs
+    """
+    
+    trios = data['TRIO ID'].str.replace('*', '').str.upper()
+    person_ids = data['Child ID'].str.replace('*', '').str.upper()
+    phenos = data['PHENO']
+    
+    # get a set of IDs that match the Coriell IDs
+    # strip off the pheno and trio info
+    cleaned = []
+    for pheno, person_id, trio in zip(phenos, person_ids, trios):
+        if person_id == trio:
+            cleaned.append(person_id)
+        else:
+            person_id = person_id.lstrip(pheno)
+            person_id = person_id.rsplit(trio, 1)[0]
+            cleaned.append(person_id)
+    
+    # fix up one ID where the paternal ID has been used instead
+    cleaned = [ x if x != 'ND29866' else 'ND29865' for x in cleaned ]
+    
+    return cleaned
+
 def epi4k_ashg_de_novos():
     """ get de novo data for the Epi4K epilepsy exome study
     
@@ -28,18 +52,9 @@ def epi4k_ashg_de_novos():
     data['chrom'], data['pos'], data['ref'], data['alt'] = fix_coordinates_with_allele( \
         data['hg19 coordinates (chr:position)'], data["Ref/Alt alleles"])
     
-    data['TRIO ID'] = data['TRIO ID'].str.replace('*', '')
-    data['person_id'] = data['Child ID'].str.replace('*', '').str.upper()
-    
     data['study'] = "10.1016/j.ajhg.2014.08.013"
     
-    # get a set of IDs that match the Coriell IDs
-    pat = re.compile("ND[0-9]*")
-    person_ids = list(data.person_id)
-    matches = ( pat.search(x) for x in person_ids )
-    person_ids = [ m.group() if m is not None else person_ids[i] for i, m in enumerate(matches) ]
-    
-    data['person_id'] = person_ids
+    data['person_id'] = get_person_ids(data)
     data['person_id'] += '|epi4k'
     data['confidence'] = 'high'
     
