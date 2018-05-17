2d9faec4142e5b0157ada0cf9ddeed26e261e806
Jeremy McRae <jmcrae@illumina.com>
2018-05-17 14:53:48 -0700

    datasets as lists of python objects, instead of pandas tables

--- dnm_cohorts/open_data.py
+++ dnm_cohorts/open_data.py
@@ -1,16 +1,23 @@
 
+import gzip
 from pkg_resources import resource_filename
 
-import pandas
+# import pandas
+from dnm_cohorts.de_novo import DeNovo
+from dnm_cohorts.person import Person
 
 DE_NOVO_PATH = resource_filename(__name__, "data/de_novos.txt.gz")
 COHORT_PATH = resource_filename(__name__, "data/cohort.txt.gz")
 
 def open_de_novos():
-    return pandas.read_table(DE_NOVO_PATH)
+    with gzip.open(DE_NOVO_PATH, 'rt') as handle:
+        header = handle.readline()
+        return [ DeNovo(*x.strip('\n').split('\t')) for x in handle ]
 
 def open_cohort():
-    return pandas.read_table(COHORT_PATH)
+    with gzip.open(COHORT_PATH, 'rt') as handle:
+        header = handle.readline()
+        return [ Person(*x.strip('\n').split('\t')) for x in handle ]
 
 de_novos = open_de_novos()
 cohort = open_cohort()
