d1a45b9bcb3fc8532e17f32a736699cf112b394a
Jeremy McRae <jmcrae@illumina.com>
2022-09-26 17:11:53 -0700

    format long list into separate lines per entry

--- dnm_cohorts/ensembl.py
+++ dnm_cohorts/ensembl.py
@@ -7,22 +7,46 @@ import trio
 REQS_PER_SECOND = 15
 
 # consequence list, as sorted at
-# http://www.ensembl.org/info/genome/variation/predicted_data.html
-consequences = ["transcript_ablation", "splice_donor_variant",
-    "splice_acceptor_variant", "stop_gained", "frameshift_variant",
-    "start_lost", "initiator_codon_variant", "stop_lost",
-    "transcript_amplification", "inframe_insertion", "inframe_deletion",
-    "missense_variant", "protein_altering_variant", "splice_region_variant",
-    "incomplete_terminal_codon_variant", "start_retained_variant",
-    "stop_retained_variant", "synonymous_variant", "coding_sequence_variant",
-    "mature_miRNA_variant", "5_prime_UTR_variant", "3_prime_UTR_variant",
-    "non_coding_transcript_exon_variant", "intron_variant",
-    "NMD_transcript_variant", "non_coding_transcript_variant",
-    "upstream_gene_variant", "downstream_gene_variant", "TFBS_ablation",
-    "TFBS_amplification", "TF_binding_site_variant",
-    "regulatory_region_ablation", "regulatory_region_amplification",
-    "regulatory_region_variant", "feature_elongation", "feature_truncation",
-    "intergenic_variant"]
+# https://www.ensembl.org/info/genome/variation/prediction/predicted_data.html
+consequences = [
+    "transcript_ablation",
+    "splice_donor_variant",
+    "splice_acceptor_variant",
+    "stop_gained",
+    "frameshift_variant",
+    "start_lost",
+    "initiator_codon_variant",
+    "stop_lost",
+    "transcript_amplification",
+    "inframe_insertion",
+    "inframe_deletion",
+    "missense_variant",
+    "protein_altering_variant",
+    "splice_region_variant",
+    "incomplete_terminal_codon_variant",
+    "start_retained_variant",
+    "stop_retained_variant",
+    "synonymous_variant",
+    "coding_sequence_variant",
+    "mature_miRNA_variant",
+    "5_prime_UTR_variant",
+    "3_prime_UTR_variant",
+    "non_coding_transcript_exon_variant",
+    "intron_variant",
+    "NMD_transcript_variant",
+    "non_coding_transcript_variant",
+    "upstream_gene_variant",
+    "downstream_gene_variant",
+    "TFBS_ablation",
+    "TFBS_amplification",
+    "TF_binding_site_variant",
+    "regulatory_region_ablation",
+    "regulatory_region_amplification",
+    "regulatory_region_variant",
+    "feature_elongation",
+    "feature_truncation",
+    "intergenic_variant",
+    ]
 severity = dict(zip(consequences, range(len(consequences))))
 
 def get_base_url(build):
