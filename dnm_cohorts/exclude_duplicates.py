e221e6d74a0e36974479dcb7db334c58a9e258c4
Jeremy McRae <jmcrae@illumina.com>
2022-09-28 07:37:09 -0700

    reuse consequence severity from another module, deduplicates list

--- dnm_cohorts/exclude_duplicates.py
+++ dnm_cohorts/exclude_duplicates.py
@@ -1,23 +1,7 @@
 
 from itertools import groupby
 
-consequences = ["transcript_ablation", "splice_donor_variant",
-    "splice_acceptor_variant", "stop_gained", "frameshift_variant",
-    "initiator_codon_variant", "stop_lost", "start_lost",
-    "transcript_amplification", 'conserved_exon_terminus_variant',
-    "inframe_insertion", "inframe_deletion", "missense_variant",
-    "protein_altering_variant", "splice_region_variant",
-    "incomplete_terminal_codon_variant", "stop_retained_variant",
-    "synonymous_variant", "coding_sequence_variant", "mature_miRNA_variant",
-    "5_prime_UTR_variant", "3_prime_UTR_variant", "non_coding_exon_variant",
-    "non_coding_transcript_exon_variant", "intron_variant",
-    "NMD_transcript_variant", "non_coding_transcript_variant",
-    "nc_transcript_variant", "upstream_gene_variant", "downstream_gene_variant",
-    "TFBS_ablation", "TFBS_amplification", "TF_binding_site_variant",
-    "regulatory_region_ablation", "regulatory_region_amplification",
-    "regulatory_region_variant", "feature_elongation", "feature_truncation",
-    "intergenic_variant"]
-severity = dict(zip(consequences, range(len(consequences))))
+from dnm_cohorts.ensembl import severity
 
 def drop_inperson_duplicates(de_novos):
     """ get independent mutation events per person
