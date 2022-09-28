672ca6ae3ef08ff8b9ea4c3ac7f765a7cee70f8d
Jeremy McRae <jmcrae@illumina.com>
2022-09-28 13:59:19 -0700

    fix logging statement

--- dnm_cohorts/de_novos/yuen_nature_neuroscience.py
+++ dnm_cohorts/de_novos/yuen_nature_neuroscience.py
@@ -8,12 +8,12 @@ from dnm_cohorts.de_novo import DeNovo
 url = 'https://static-content.springer.com/esm/art%3A10.1038%2Fnn.4524/MediaObjects/41593_2017_BFnn4524_MOESM49_ESM.xlsx'
 
 async def yuen_nature_neuroscience_de_novos(result):
-    """ gets individual level data for Yuen et al
+    """ gets individual level data for Yuen et al 2017
     
     Supplementary Table 3 from:
     Yuen et al. Nature Neturoscience 2017, doi: 10.1038/nn.4524
     """
-    logging.info('getting Fu et al Nature Genetics 2022 de novos')
+    logging.info('getting Yuen et al Nature Neuroscience 2017 de novos')
     data = pandas.read_excel(url, 'Table S3', skiprows=1)
     
     data['person_id'] = data['SAMPLE']
