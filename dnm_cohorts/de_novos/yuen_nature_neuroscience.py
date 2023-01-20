09ab4a2753af8e58524d8f2e67face83c4382e9a
Jeremy McRae <jmcrae@illumina.com>
2023-01-20 10:07:55 -0800

    better paper referencing

--- dnm_cohorts/de_novos/yuen_nature_neuroscience.py
+++ dnm_cohorts/de_novos/yuen_nature_neuroscience.py
@@ -11,7 +11,8 @@ async def yuen_nature_neuroscience_de_novos(result):
     """ gets individual level data for Yuen et al 2017
     
     Supplementary Table 3 from:
-    Yuen et al. Nature Neturoscience 2017, doi: 10.1038/nn.4524
+    Yuen et al. Nature Neturoscience 2017, 20:602-611
+    doi: 10.1038/nn.4524
     """
     logging.info('getting Yuen et al Nature Neuroscience 2017 de novos')
     data = pandas.read_excel(url, 'Table S3', skiprows=1)
